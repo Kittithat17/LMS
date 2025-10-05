406d80dfe1fc254757d1aaeaccd91193ed96632f
Kittithat17 <hdhsdhdhdd1@gmail.com>
2025-10-06 03:53:00 +0700

    sending email otp

--- lib/auth.ts
+++ lib/auth.ts
@@ -8,22 +8,23 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql", // or "mysql", "postgresql", ...etc
   }),
+  plugins: [
+    emailOTP({
+      async sendVerificationOTP({ email, otp }) {
+        await resend.emails.send({
+          from: "LearnPath <onboarding@resend.dev>",
+          to: [email],
+          subject: "LearnPath Login OTP",
+          html: `<p>Your OTP code is: <strong>${otp}</strong></p>`,
+        });
+      },
+    }),
+  ],
   socialProviders: {
     google: {
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
     },
-    plugins: [
-      emailOTP({
-        async sendVerificationOTP({ email, otp }) {
-           await resend.emails.send({
-            from: "LearnPath <onboarding@resend.dev>",
-            to: [email],
-            subject: "LearnPath Login OTP",
-            html: `<p>Your OTP code is: <strong>${otp}</strong></p>`,
-          });
-        },
-      }),
-    ],
+    
   },
 });
